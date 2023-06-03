9271a79308ae751b853cf900a107577b04286d38
Andrey.Lykosov <alykosov2@gmail.com>
2023-06-03 21:51:25 +0300

    New

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
-
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
         int a = 5;
         int b = 4;
         int c = a + b;
