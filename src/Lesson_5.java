03533b07456dc8ce2de0194e0064286fc0b6e1cf
Andrey.Lykosov <alykosov2@gmail.com>
2023-06-07 21:31:30 +0300

    New

--- src/Lesson_5.java
+++ src/Lesson_5.java
@@ -4,9 +4,9 @@ public class Lesson_5
 {
     public static void main (String[] args ) {//Создаем класс
         Scanner scanner = new Scanner(System.in);//Cоздаём метод scanner
-        System.out.println("Enter а (0...10):");//Печатает запрос на ввод числа а
+        System.out.println("Enter а:");//Печатает запрос на ввод числа а
         int a = scanner.nextInt();//сканируем число a
-        System.out.println("Enter b (0..10):");//Печатает запрос на ввод числа b
+        System.out.println("Enter b:");//Печатает запрос на ввод числа b
         int b = scanner.nextInt();//сканируем число b
 
         if (a < b) {//Еслм а<b, выводим на печать
