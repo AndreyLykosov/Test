4a7810c07c1d6d7bb50a9fdaddab13a75d8cb8a5
Andrey.Lykosov <alykosov2@gmail.com>
2023-06-10 20:29:19 +0300

    New

---
+++ src/Lesson_6.java
@@ -0,0 +1,15 @@
+public class Lesson_6 {
+    public static void main(String[] args) {//Создаем класс
+        System.out.println("Числа от 1 до 10");
+        for (int A = 1; A < 11; A++) {
+            System.out.println(A);
+        }
+    }
+        private static void Double_numbers() {
+            System.out.println("Парные числа от 1 до 100");
+            for (int B = 1; B < 100; B++)
+                if (B % 2 == 0) {
+                System.out.println(B);
+                }
+        }
+    }
