4d171110aeae43a9de316aeb0e9e6df676390a6f
Andrey.Lykosov <alykosov2@gmail.com>
2023-07-30 17:12:48 +0300

    New

--- src/Lesson_16.java
+++ src/Lesson_16.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 public class Lesson_16 {
+    static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+        ArrayList<Integer> numbersArrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numbersArrayList.add(i);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("arraylist" + " " + (end-start));
+
+    }
    
 }
