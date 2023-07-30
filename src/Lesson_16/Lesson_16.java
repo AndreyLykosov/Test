92b371bd7c3b1dc74b22d1880934229fb97fe7ec
Andrey.Lykosov <alykosov2@gmail.com>
2023-07-30 18:33:12 +0300

    New

--- src/Lesson_16/Lesson_16.java
+++ src/Lesson_16/Lesson_16.java
@@ -1,39 +1,28 @@
 package Lesson_16;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
 public class Lesson_16 {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         arrayList();
         linkedList();
-
-        private static void arrayList () {
-
-            List<Integer> integerList = new ArrayList<>();
-            int i;
-            for (i = 1; i <= 10; i++) {
-                integerList.add(i);
-                {
-                    System.out.println("ArrayList" + integerList.get(i));
-                }
-            }
-        }
     }
 
     private static void arrayList() {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            integerList.add(i);
+            System.out.println("ArrayList " + integerList.get(i - 1));
+        }
     }
 
     private static void linkedList() {
         List<Integer> numbers = new LinkedList<>();
-        int i;
-        for (i = 1; i <= 2000000; i++) {
+        for (int i = 1; i <= 2000000; i++) {
             numbers.add(i);
-            {
-                System.out.println("LinkedList" + numbers.get(i));
-            }
-
+            System.out.println("LinkedList " + numbers.get(i - 1));
         }
     }
 }
