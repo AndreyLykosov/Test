868bcaa2766379d246ea5647efe68dc954f5300d
Andrey.Lykosov <alykosov2@gmail.com>
2023-09-17 20:49:06 +0300

    New

---
+++ src/Lesson_33/Note.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+public class Note {
+    private String text;
+    private String author;
+    private Date createdAt;
+
+    public Note(String text, String author) {
+        this.text = text;
+        this.author = author;
+        this.createdAt = new Date();
+    }
+
+    // Геттери та сеттери
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "text='" + text + '\'' +
+                ", author='" + author + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
+
